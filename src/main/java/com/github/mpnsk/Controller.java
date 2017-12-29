a82a15c0e331157c0a51974f0f0f5b6ef2d3d00d
mpnsk <mpnsk@users.noreply.github.com>
2017-12-30 00:08:56 +0100

    working basic stack-based listener setup

--- src/main/java/com/github/mpnsk/Controller.java
+++ src/main/java/com/github/mpnsk/Controller.java
@@ -1,68 +1,93 @@
 package com.github.mpnsk;
 
-import javafx.event.Event;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
 
 import java.util.Stack;
 
 public class Controller {
-    public Button button1, forward, backward;
     public Pane pane;
-    public CheckBox checkbox;
     public Label label;
+    public Circle circle;
+    public Rectangle rectangle;
     private Runnable stage1;
+    private Line line;
 
     public void initialize() {
 
-
         Stack<Runnable> stack = new Stack<>();
 
-        Runnable stage4 = () -> {
-            label.setText("stage 4");
-            button1.setOnMouseClicked(event -> {
-                button1.setOnMouseClicked(Event::consume);
-                stack.empty();
-                stack.push(stage1).run();
-            });
-        };
-
-        Runnable stage3 = () -> {
-            label.setText("stage3");
-            forward.setOnMouseClicked(event -> {
-                forward.setOnMouseClicked(Event::consume);
-                stack.push(stage4).run();
-            });
-            backward.setOnMouseClicked(event -> {
-                backward.setOnMouseClicked(Event::consume);
+        Runnable rectangleMouseEntered = () -> {
+            highlightRectangle();
+            label.setText("rectangleMouseEntered");
+            rectangle.setOnMouseExited(event -> {
+                rectangle.setOnMouseExited(null);
+                System.out.println(event.getEventType());
+                assert line != null;
+                removeLine(line);
                 stack.pop();
                 stack.peek().run();
-
             });
         };
-        Runnable stage2 = () -> {
-            label.setText("stage2");
-            forward.setOnMouseClicked(event -> {
-                forward.setOnMouseClicked(Event::consume);
-                stack.push(stage3).run();
+        Runnable s2DraggingLine = () -> {
+            label.setText("s2DraggingLine");
+            line = createLine();
+            rectangle.setOnMouseEntered(event -> {
+                rectangle.setOnMouseEntered(null);
+                System.out.println(event.getEventType());
+                stack.push(rectangleMouseEntered).run();
+            });
+            pane.setOnMouseClicked(event -> {
+                pane.setOnMouseClicked(null);
+                rectangle.setOnMouseEntered(null);
+                removeLine(line);
+                System.out.println(event.getEventType());
+                label.setText("no stage");
+                stack.empty();
+                assert stage1 != null;
+                stack.push(stage1).run();
             });
         };
         stage1 = () -> {
             label.setText("stage1");
-            button1.setOnMouseClicked(event -> {
-                button1.setOnMouseClicked(Event::consume);
-                stack.push(stage2).run();
+            circle.setOnMouseClicked(event -> {
+                circle.setOnMouseClicked(null);
+                event.consume();
+                System.out.println(event.getEventType());
+                stack.push(s2DraggingLine).run();
             });
 
         };
-        stack.add(stage1);
-        stack.peek().run();
+        stack.push(stage1).run();
     }
 
-    public void mousePressed(MouseEvent mouseEvent) {
-
+    private void highlightRectangle() {
+        rectangle.setHeight(rectangle.getHeight() + 15);
+        rectangle.setWidth(rectangle.getWidth() + 15);
     }
+
+    private void removeLine(Line line) {
+        line.startXProperty().unbind();
+        line.startYProperty().unbind();
+        pane.getChildren().remove(line);
+        pane.setOnMouseMoved(null);
+    }
+
+    private Line createLine() {
+        Line line = new Line();
+        line.setStrokeWidth(3);
+        line.setMouseTransparent(true);
+        line.startXProperty().bind(circle.centerXProperty().add(circle.getLayoutX()));
+        line.startYProperty().bind(circle.centerYProperty().add(circle.getLayoutY()));
+        pane.setOnMouseMoved(event -> {
+            line.setEndX(event.getSceneX());
+            line.setEndY(event.getSceneY());
+        });
+        pane.getChildren().add(line);
+        return line;
+    }
+
 }
